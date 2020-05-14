faccc8e022d1f9ebcb4773395f77f18073f7a0d2
Andrew Lai <andrew.s.lai5@gmail.com>
2020-05-14 09:51:19 -0500

    Add cider-eval-around-point

--- +bindings.el
+++ +bindings.el
@@ -16,12 +16,33 @@
       :desc "magit status" :n "g s" #'magit
       :desc "lisp-state" :n "k" #'lisp-state-toggle-lisp-state)
 
-;; Cider
+(defun cider-eval-around-point ()
+  (interactive)
+  ;; like save-excursion, but we need to set the marker type to 't
+  (let ((m (point-marker)))
+    (set-marker-insertion-type m 't)
+    (lisp-state-prev-opening-paren)
+    (cider-eval-sexp-at-point)
+    (goto-char m)))
+
+;; Cider keybindings
 (map! :after clojure-mode
       :map clojure-mode-map
-      :n ", e n" #'cider-eval-sexp-at-point)
+      :n ", e a" #'cider-eval-around-point
+      :n ", e b" #'cider-eval-buffer
+      :n ", e c" #'cider-pprint-eval-last-sexp-to-comment
+      :n ", e e" #'cider-eval-last-sexp
+      :n ", e f" #'cider-eval-defun-at-point
+      :n ", e n" #'cider-eval-sexp-at-point
+      :n ", e l" #'cider-eval-last-sexp
+      :n ", t n" #'cider-test-run-ns-tests
+      :n ", t p" #'cider-test-run-project-tests
+      :n ", s s" #'cider-switch-to-repl-buffer
+      :n ", '" #'cider-jack-in-clj
+      )
 
 
+;; LISP state keybindings
 (define-key evil-lisp-state-map "c" 'sp-convolute-sexp)
 (define-key evil-lisp-state-map "dx" 'sp-kill-sexp)
 (define-key evil-lisp-state-map "e" 'sp-splice-sexp-killing-forward)
