8fc60370ce91f2df69a745caaab9852ef6913d12
Andrew Lai <andrew.s.lai5@gmail.com>
2020-05-19 17:16:35 -0500

    Add todo note

--- config.el
+++ config.el
@@ -3,10 +3,10 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-;; TODO: Undo tree? For being able to undo autoindenting changes
 ;; TODO: Keybinding for newline and indent? S-RET?
 ;; TODO: Make sure insert always inserts balanced parens
-;; TODO:
+;; TODO: Write fn that goes to beginning of line then does an
+;;       evil-sp-delete-line
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
